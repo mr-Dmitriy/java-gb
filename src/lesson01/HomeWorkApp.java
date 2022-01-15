6b54091aa91aaa18d1ced79901eed47a650a1773
Dmitriy_gb <d.o.prihodchenko@gmail.com>
2022-01-15 15:23:42 +0300

    ДЗ-1 исправленно

--- src/lesson01/HomeWorkApp.java
+++ src/lesson01/HomeWorkApp.java
@@ -7,7 +7,7 @@
 
 
 package lesson01;
-import java.util.Scanner;
+
 
 public class HomeWorkApp {
 
@@ -23,44 +23,28 @@ public class HomeWorkApp {
     }
 
     static void checkSumSign() {
-        int a = 0;
-        int b = 0;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("a = ");
-        a = sc.nextInt();
-        System.out.println("b = ");
-        b = sc.nextInt();
+        int a = 2;
+        int b = 3;
         System.out.println(((a + b) >= 0)? "Положительная" : "Отрицательная");
     }
 
     static void printColor() {
-
-        Scanner sc = new Scanner(System.in);
-            int value = 0;
-            System.out.println("value = ");
-            value = sc.nextInt();
-
-            if ( value <= 0 ) {
-                System.out.println("RED");
-            }
-            else if ( value > 0 && value <=100 ) {
-                System.out.println("YELLOW");
-            }
-            else if ( value > 100 ) {
-                System.out.println("GREEN");
-            }
-
+        int value = 5;
+        if ( value <= 0 ) {
+            System.out.println("RED");
+        }
+        else if ( value > 0 && value <=100 ) {
+            System.out.println("YELLOW");
+        }
+        else if ( value > 100 ) {
+            System.out.println("GREEN");
+        }
     }
 
-     static void compareNumbers() {
-        Scanner sc = new Scanner(System.in);
-            int a = 0;
-            int b = 0;
-            System.out.println("a = ");
-            a = sc.nextInt();
-            System.out.println("b = ");
-            b = sc.nextInt();
-            System.out.println( a >= b ? "a >= b" : "a < b");
+    static void compareNumbers() {
+        int a = 5;
+        int b = 7;
+        System.out.println( a >= b ? "a >= b" : "a < b");
     }
 
 }
